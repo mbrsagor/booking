6df9d669a4ca6c30deac435d80ec75e3ff9a9948
mbrsagor <mbrsagor@gmail.com>
2021-12-30 22:46:03 +0600

    New: Status filter added

--- rent/filters.py
+++ rent/filters.py
@@ -5,4 +5,4 @@ from rent.models import Booking
 class BookingFilter(django_filters.FilterSet):
     class Meta:
         model = Booking
-        fields = ['phone_number', 'transaction_id', 'booking_date', ]
+        fields = ['phone_number', 'transaction_id', 'booking_date', 'status']
