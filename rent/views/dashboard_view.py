fe838138c83e86eec224c1e9a80e663554b7b4bf
mbrsagor <mbrsagor@gmail.com>
2021-12-31 22:30:05 +0600

    Refactor: Dashboard refactor

--- rent/views/dashboard_view.py
+++ rent/views/dashboard_view.py
@@ -15,14 +15,23 @@ class DashboardView(TemplateView):
     def dispatch(self, *args, **kwargs):
         return super(DashboardView, self).dispatch(*args, **kwargs)
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(DashboardView, self).get_context_data(**kwargs)
-        context['booking'] = Booking.objects.all().order_by('-id')
-        context['cancel_booking'] = Booking.objects.filter(status=0)
-        context['pending_booking'] = Booking.objects.filter(status=1)
-        context['users'] = User.objects.all()
-        context['locations'] = Location.objects.all()
-        context['rents'] = Rent.objects.all()
-        return context
+    def get(self, request, *args, **kwargs):
+        booking = Booking.objects.all().order_by('-id')
+        cancel_booking = Booking.objects.filter(status=0)
+        pending_booking = Booking.objects.filter(status=1)
+        users = User.objects.all()
+        locations = Location.objects.all()
+        rents = Rent.objects.all()
+        booking_status = Booking.objects.filter(customer=self.request.user)
+        context = {
+            'booking': booking,
+            'users': users,
+            'locations': locations,
+            'rents': rents,
+            'cancel_booking': cancel_booking,
+            'pending_booking': pending_booking,
+            'booking_status': booking_status,
+        }
+        return self.render_to_response(context)
 
     template_name = 'dashboard/dashboard.html'
