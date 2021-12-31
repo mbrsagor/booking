86c0d83869502735642edcfa5cf7ceb46e2ed33f
mbrsagor <mbrsagor@gmail.com>
2021-12-31 23:32:28 +0600

    New: Homepage search filter working

--- rent/filters.py
+++ rent/filters.py
@@ -1,5 +1,13 @@
 import django_filters
-from rent.models import Booking
+from rent.models import Rent, Booking
+
+
+class RentFilter(django_filters.FilterSet):
+    class Meta:
+        model = Rent
+        fields = [
+            'name', 'bed_room', 'bath_room', 'rent_location', 'types'
+        ]
 
 
 class BookingFilter(django_filters.FilterSet):
