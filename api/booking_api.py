0a78a514cf1694c5e0f922cd58341f1fddf67876
mbrsagor <mbrsagor@gmail.com>
2022-01-19 16:50:51 +0600

    New: Booking update, details, and delete operation done

--- api/booking_api.py
+++ api/booking_api.py
@@ -2,8 +2,7 @@ from rest_framework import views, serializers, status, permissions
 from rest_framework.response import Response
 
 from rent.models import Booking
-from rent.utils.responses import prepare_success_response, prepare_error_response
-from rent.utils.validate_service import validate_booking_service
+from rent.utils.responses import prepare_success_response, prepare_error_response, prepare_create_success_response
 
 
 class BookingSerializer(serializers.ModelSerializer):
@@ -16,7 +15,7 @@ class BookingSerializer(serializers.ModelSerializer):
 
 
 class BookingAPIListCreateView(views.APIView):
-    permission_classes = [permissions.IsAdminUser, ]
+    permission_classes = [permissions.IsAuthenticated, ]
 
     def get(self, request):
         location = Booking.objects.all()
@@ -24,11 +23,44 @@ class BookingAPIListCreateView(views.APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        # validate_error = validate_booking_service(request.data)
-        # if validate_error is not None:
-        #     return Response(prepare_error_response(validate_error), status=status.HTTP_400_BAD_REQUEST)
         serializer = BookingSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(customer=request.user)
             return Response(prepare_success_response(serializer.data), status=status.HTTP_200_OK)
         return Response(prepare_error_response(serializer.errors), status=status.HTTP_400_BAD_REQUEST)
+
+
+class BookingUpdateDetailAPI(views.APIView):
+    permission_classes = [permissions.IsAdminUser, ]
+
+    def get_object(self, pk):
+        try:
+            return Booking.objects.get(id=pk).first()
+        except Booking.DoesNotExist:
+            return None
+
+    def put(self, request, pk):
+        booking = self.get_object(pk)
+        if booking is not None:
+            serializer = BookingSerializer(booking, data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(prepare_create_success_response(serializer.data), status=status.HTTP_201_CREATED)
+            return Response(prepare_error_response(serializer.errors), status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(prepare_error_response("No data found for this ID"), status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, pk):
+        booking = self.get_object(pk)
+        serializer = BookingSerializer(booking)
+        if serializer is not None:
+            return Response(prepare_success_response(serializer.data), status=status.HTTP_200_OK)
+        return Response(prepare_error_response("Content Not found"), status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        booking = self.get_object(pk)
+        if booking is not None:
+            booking.delete()
+            return Response(prepare_success_response("Data deleted successfully"), status=status.HTTP_200_OK)
+        else:
+            return Response(prepare_error_response("Content Not found"), status=status.HTTP_400_BAD_REQUEST)
--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from api.location_api import LocationAPIListCreateView
 from api.rent_api import RentAPIListCreateView, RentUpdateDetailDeleteAPIView
-from api.booking_api import BookingAPIListCreateView
+from api.booking_api import BookingAPIListCreateView, BookingUpdateDetailAPI
 
 urlpatterns = [
     path('location/', LocationAPIListCreateView.as_view()),
     path('rent/', RentAPIListCreateView.as_view()),
     path('rent/<pk>/', RentUpdateDetailDeleteAPIView.as_view()),
     path('booking/', BookingAPIListCreateView.as_view()),
+    path('booking/<pk>/', BookingUpdateDetailAPI.as_view()),
 ]
