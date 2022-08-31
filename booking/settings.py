e79e821bf2ac49193efafb1210e35e121b9b7df6
mbrsagor <mbrsagor@gmail.com>
2022-08-31 12:57:30 +0600

    New: add deployment commit.

--- booking/settings.py
+++ booking/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 env = environ.Env(
     DEBUG=(bool, False),
-    ALLOWED_HOSTS=(list),
+    ALLOWED_HOSTS=list,
 )
 env_path = os.path.join(BASE_DIR, '.env')
 environ.Env.read_env(env_path)
